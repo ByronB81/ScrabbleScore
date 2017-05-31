b4cd42849cdf68bbdc34e4c794c23fd606a5eecd
Byron Blocker <byronblocker@gmail.com>
2017-05-30 18:04:17 -0700

    add front end logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,16 @@
+import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
+    Console console = System.console();
+    ScrabbleScore userScrabble = new ScrabbleScore();
 
     System.out.println("Let's figure out your Scrabble score!");
+    System.out.println("please enter a word");
+    String userWord = console.readLine();
+    String finalScore = userScrabble.calculateScore(userWord);
+
+    System.out.println("The word " + userWord + " is worth " + finalScore + " points.");
 
   }
 }
